17a62fdce392c6770b8cc95218851877a9dba0b7
Chunt0 <chunt.mail@gmail.com>
2023-07-14 11:15:49 -0700

    2023-07-14 11:15:49

--- Code/graph.py
+++ Code/graph.py
@@ -1,26 +1,26 @@
 import matplotlib.pyplot as plt
 
+def read_data(file_path):
+    x_values = []
+    y_values = []
 
-def read_data(filename):
-    x = []
-    y = []
-    count = 0
-    with open(filename, 'r') as file:
+    with open(file_path, 'r') as file:
         for line in file:
-            count += 1
-            val = int(line.strip())
-            x.append(count)
-            y.append(val)
+            data = line.strip().split('\t')
+            if len(data) == 2:
+                data[0] = (2/1023)*float(data[0]) - 1
+                data[1] = ((4/1023)*float(data[1]) - 2)/12500*10**6
+                x_values.append(data[0])
+                y_values.append(data[1])
 
-    return x, y
+    return x_values, y_values
 
-PATH = 'data.txt'
+data_file = 'data.txt'  # Update with your file path
+x, y = read_data(data_file)
 
-x, y = read_data(PATH)
-
-plt.scatter(x,y)
-plt.xlabel('Time [t] (s)')
-plt.ylabel('Voltage [V] (v)')
-plt.title('Voltage vs. Time')
+plt.plot(x, y, 'o')
+plt.xlabel('V_in')
+plt.ylabel('I_out')
+plt.title('Data Plot')
+plt.grid(True)
 plt.show()
-
